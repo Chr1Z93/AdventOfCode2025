72a2f3be4b318402a963625ecc2bccebf7496ffc
Chr1Z93 <puls.christian@gmail.com>
2025-12-12 10:54:32 +0100

    day12

--- Day12/part1.py
+++ Day12/part1.py
@@ -1,18 +1,16 @@
 # Problem description:
-#
-#
-#
-#
+# Compare area of presents to tree
 
 from pathlib import Path
 import time
+import math
 
 # get correct subfolder path
 script_path = Path(__file__).resolve()
 script_dir = script_path.parent
 
-#input_path = script_dir / "input.txt"
-input_path = script_dir / "example.txt"
+input_path = script_dir / "input.txt"
+# input_path = script_dir / "example.txt"
 
 input_file = open(input_path)
 
@@ -20,9 +18,26 @@ input_file = open(input_path)
 def get_answer():
     answer = 0
 
+    i = -1
     for line in input_file:
-        # do something, example:
-        answer += 1
+        i += 1
+
+        # Skip first part
+        if i < 30:
+            continue
+
+        parts = line.split(":")
+        sizes = parts[0].split("x")
+        counts = parts[1].strip().split(" ")
+
+        max_count = math.floor(int(sizes[0]) / 3) * math.floor(int(sizes[1]) / 3)
+
+        current_count = 0
+        for count in counts:
+            current_count += int(count)
+
+        if current_count <= max_count:
+            answer += 1
 
     return answer
 
